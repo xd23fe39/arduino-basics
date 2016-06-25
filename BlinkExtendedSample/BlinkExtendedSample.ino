50086b713cfc2ff6b132c9a26efb444ce0a847ee
xd23fe39 <xd23fe39@yahoo.de>
2016-06-25 17:07:10 +0200

    Initial commit

---
+++ BlinkExtendedSample/BlinkExtendedSample.ino
@@ -0,0 +1,57 @@
+/*
+  Blink
+  Turns on an LED on for one second, then off for one second, repeatedly.
+ 
+  This example code is in the public domain.
+ */
+
+#define BUILTIN_LED  13
+ 
+/**
+ * Defines a simple Component using a timer instead delay. 
+ */
+class Blink {
+private:
+  int ledPin;
+  int interval = 1500;
+  int value = LOW;
+  int timer = 0;
+public:
+  Blink(int pin = BUILTIN_LED, int interval = 1500) {
+    this->ledPin = pin;
+    this->timer = 0;
+    this->interval = interval;
+    this->value = LOW;
+  }
+  
+  void setup() {
+    pinMode(ledPin, OUTPUT);
+  }
+  
+  void toggle() {
+      value = ( value == HIGH ) ? LOW : HIGH;
+      digitalWrite(ledPin, value);
+  }
+  
+  void looper() {
+    if ( ++timer >= interval ) {
+      timer = 0;
+      toggle();
+    }
+  }
+  
+};
+
+Blink blink(BUILTIN_LED);
+
+// the setup routine runs once when you press reset:
+void setup() {                
+  // initialize the digital pin as an output.
+  blink.setup();
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  blink.looper();
+  delay(1);
+}
