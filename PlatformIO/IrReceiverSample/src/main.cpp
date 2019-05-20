da73eabb7730a7654918a415dfb68a05de1ce898
xd23fe39 <xd23fe39@yahoo.de>
2019-05-20 20:13:20 +0200

    Hinzufügen von SignalLED

--- PlatformIO/IrReceiverSample/src/main.cpp
+++ PlatformIO/IrReceiverSample/src/main.cpp
@@ -10,6 +10,7 @@
 
 #include <Arduino.h>
 #include <IRremote.h>
+#include <SignalLED.h>
 
 // Counting ticks
 unsigned long counter = 0;
@@ -24,51 +25,8 @@ decode_results results;
 
 unsigned long ir_undefined;
 
-/**
- * Lässt die LED auf Pin pin cnt-Mal blinken.
- */
-void blink(int pin, int cnt, int rate = 500) {
-  digitalWrite(pin, LOW);
-  delay(LATENCY);
-  for (int i = 0; i < cnt; i++) {
-    digitalWrite(pin, HIGH);
-    delay(rate);
-    digitalWrite(pin, LOW);
-    delay(rate);
-  }
-}
-
-/**
- * Lässt die LED auf Pin pin 'hektisch' blinken.
- */
-void panik(int pin, int cnt = 10, int rate = 50) {
-  blink(pin, cnt, rate);
-}
-
-void alive(int pin, int rate = 50) {
-  digitalWrite(pin, LOW);
-  delay(LATENCY);
-  digitalWrite(pin, HIGH);
-  delay(rate);
-  digitalWrite(pin, LOW);
-}
-
-void start(int pin, int rate = 800) {
-  digitalWrite(pin, LOW);
-  delay(LATENCY);
-  digitalWrite(pin, HIGH);
-  delay(rate);
-  digitalWrite(pin, LOW);
-  delay(500);
-  digitalWrite(pin, HIGH);
-  delay(rate);
-  digitalWrite(pin, LOW);
-  delay(500);
-  digitalWrite(pin, HIGH);
-  delay(200);
-  digitalWrite(pin, LOW);
-  delay(WAIT);
-}
+// Initialize SignalLED using PIN 6 (GND on PIN 5)
+SignalLED signalLED(6, 5);
 
 void reset() {
   ir_undefined = 0;
@@ -78,20 +36,10 @@ void reset() {
 void setup() {
   Serial.begin(9600);
 
-  // Set LED pin mode
-  pinMode(5, OUTPUT);
-  pinMode(6, OUTPUT);
-  pinMode(7, OUTPUT);
-
   // Set pin mode for IR
   pinMode(9, OUTPUT);     // IR +5V
   pinMode(10, OUTPUT);    // IR gnd
   pinMode(11, OUTPUT);    // IR input (see: enableIRIn)
-
-  // Initial LED pin level
-  digitalWrite(5, LOW);
-  digitalWrite(6, LOW);
-  digitalWrite(7, LOW);
 
   // Initial pin level for IR
   digitalWrite(9, HIGH);
@@ -101,6 +49,8 @@ void setup() {
   // Initial IR in pin
   irrecv.enableIRIn();  
 
+  // First LED 'Hello' 
+  signalLED.start();
 }
 
 void loop() {
@@ -123,29 +73,28 @@ void loop() {
       break;
     case 16195807: case 1838295255:
       Serial.println("IR receive: RED Button pusched.");
-      blink(6, 2, 300);
+      signalLED.blink(2, 300);
       reset();
       break;
     case 16228447: case 1838327895:
       Serial.println("IR receive: GREEN Button pusched.");
-      blink(6, 3, 300);
+      signalLED.blink(3, 300);
       reset();
       break;
     case 1838336055: 
       Serial.println("IR receive: Start/Stop Button pusched.");
-      start(6);
+      signalLED.start();
       reset();
       break;
     default: 
-      panik(6);
+      signalLED.panik();
       Serial.println("IR receive: undefined code.");
       ir_undefined = results.value;
       break;
     }
   }
 
-  if (counter % 20 == 0)
-    alive(6);
+  signalLED.alive();
 
   delay(TICK);
 }
