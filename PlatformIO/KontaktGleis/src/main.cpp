e4295c55f1cd4909bbd6b8a24be41b7549b166ab
xd23fe39 <xd23fe39@yahoo.de>
2019-05-27 19:37:25 +0200

    Kontaktschalterklasse hinzugefügt

--- PlatformIO/KontaktGleis/src/main.cpp
+++ PlatformIO/KontaktGleis/src/main.cpp
@@ -1,29 +1,61 @@
 #include <Arduino.h>
 #include <SignalLED.h>
 
-unsigned long counter = 0;
+class Kontaktschaltung {
+
+protected:
+
+  SignalLED *signalLED;
+
+  // Counter
+  unsigned counter = 0;
+
+  // PIN Layout
+  uint8_t pin_sig = 7;
+
+public:
+
+  Kontaktschaltung(SignalLED *signalLED = NULL) {
+    this->signalLED = signalLED;
+  }
+
+  ~Kontaktschaltung() {
+    free(signalLED);
+  }
+
+  void init() {
+  }
+
+  boolean read() {
+    return digitalRead(pin_sig);    
+  }
+
+  void alive() {
+    if (read() == HIGH) {
+      if ( signalLED )
+        signalLED->blink(1, 500);
+      counter = 0;
+    }
+
+    // Zähler
+    counter++;
+  }
+
+};
 
 SignalLED signalLED(6,5);
+Kontaktschaltung schalter(&signalLED);
 
 void setup() {
   Serial.begin(9600);
+  schalter.init();
   signalLED.start();
-  pinMode(7, INPUT);     // GND
 }
 
 void loop() {
-  Serial.print("Serial monitor test: ");
-  Serial.println(counter++);
-  boolean in = digitalRead(7);
-  Serial.print("Kontaktgleis: ");
-  Serial.println(in);
-  if (digitalRead(7) == HIGH) {
-    signalLED.start();
-    counter = 0;
-    delay(200);
-    return;
-  }
-  
-  signalLED.alive(20);
+  schalter.alive();
+  signalLED.alive();
+
+  // Warte 100ms
   delay(100);
 }
