dda9f56e22be2cd976e5ca1f76d303cef1e15204
xd23fe39 <xd23fe39@yahoo.de>
2019-06-06 23:49:07 +0200

    Funksteckdose (RCModul) überarbeitet

---
+++ PlatformIO/Funksteckdose/src/SignalLED.h
@@ -0,0 +1,89 @@
+class SignalLED {
+
+  protected:
+
+    const int WAIT = 500;
+    const int LATENCY = 50;
+
+  public:
+
+    int led_pin = 6;
+    int led_gnd = 5;
+
+    long counter = 0;
+
+    SignalLED(int pin_led, int pin_gnd = 0) {
+      this->setup(pin_led, pin_gnd);
+    }
+
+    void setup(int pin_led, int pin_gnd = 0) {
+      // Initialize
+      this->led_pin = pin_led;
+      this->led_gnd = pin_gnd; 
+      // PINs for LED
+      if (pin_gnd) { 
+        pinMode(pin_gnd, OUTPUT);     // GND
+        digitalWrite(pin_gnd, LOW); 
+      }
+      pinMode(pin_led, OUTPUT);       // +5V
+      digitalWrite(pin_led, LOW);
+    }
+
+    // Lässt die LED cnt-Mal blinken
+    void blink(int cnt, int ms = 300) {
+      digitalWrite(this->led_pin, LOW);
+      delay(this->LATENCY);
+      for (int i = 0; i < cnt; i++) {
+        digitalWrite(this->led_pin, HIGH);
+        delay(ms);
+        digitalWrite(this->led_pin, LOW);
+        delay(ms);
+      }
+    }
+
+  /**
+   * Lässt die LED auf Pin pin 'hektisch' blinken.
+   */
+  void panik(int cnt = 10, int rate = 50) {
+    this->blink(cnt, rate);
+  }
+
+  /**
+   * Lässt die LED alle ticks lang kurz blinken.
+   * 
+   * alive() wird in loop() aufgerufen.
+   */
+  void alive(int tick = 20, int ms = 20) {
+    if (this->counter % tick == 0) {
+      digitalWrite(this->led_pin, LOW);
+      delay(this->LATENCY);
+      digitalWrite(this->led_pin, HIGH);
+      delay(ms);
+      digitalWrite(this->led_pin, LOW);
+    }
+    this->counter++;
+  }
+
+  void start(int rate = 800) {
+    digitalWrite(this->led_pin, LOW);
+    delay(this->LATENCY);
+    digitalWrite(this->led_pin, HIGH);
+    delay(rate);
+    digitalWrite(this->led_pin, LOW);
+    delay(500);
+    digitalWrite(this->led_pin, HIGH);
+    delay(rate);
+    digitalWrite(this->led_pin, LOW);
+    delay(500);
+    digitalWrite(this->led_pin, HIGH);
+    delay(200);
+    digitalWrite(this->led_pin, LOW);
+    delay(this->WAIT);
+    this->reset();
+  }
+
+  void reset() {
+    this->counter = 1;      // 0 würde alive-LED auslösen, was nicht gewünscht ist
+  }
+
+};
--- PlatformIO/Funksteckdose/src/main.cpp
+++ PlatformIO/Funksteckdose/src/main.cpp
@@ -7,60 +7,204 @@
 
 #include <Arduino.h>
 #include <RCSwitch.h>
+#include <SignalLED.h>
 #include <output.h>
 
-class Funksteckdose {
+// #define DEBUG
+
+struct AnalogSensor {
+  uint8_t pin;
+  int value;
+  int value_b;
+  int tolerance;
+  int delta;
+};
+
+class RCModul {
 
 protected:
 
-  RCSwitch rcswitch = RCSwitch();
+  RCSwitch rcmonitor = RCSwitch();
+  RCSwitch rcsender = RCSwitch();
+  SignalLED signalLED = SignalLED(PIN4);
+  AnalogSensor sensor;
+
   unsigned long rcvalue;
   unsigned int rcbitlen;
   unsigned int rcdelay;
   unsigned int rcprot;
   unsigned int* rcrawdata;
 
+  unsigned long rc_timer = 0;
+
+  long data_send[2] = {5263700, 5263697};
+
 public:
 
-  void init() {
-    Serial.begin(9600);
-    rcswitch.enableReceive(0);  // Receiver on interrupt 0 => that is pin #2
-    Serial.print("\nRCSwitch Funkmodul Testprogramm\n");
-    Serial.println();
+  static const int RECEIVER_INT = 0;         // Default: interrupt 0 => PIN2
+  static const int TRANSMITTER_PIN = 10;     // Default: PIN for Transmitter Data
+  static const int TRANSMITTER_VCC = PIN7;   // Default: Transmitter Standby and VCC
+  static const int SENSOR_PIN = PIN_A0;      // Default: Analog sensor
+
+  void setupTransmitter(uint8_t pin) {
+    pinMode(TRANSMITTER_VCC, OUTPUT); 
+    rcsender.enableTransmit(pin);
+    rcsender.setProtocol(1);
+    rcsender.setPulseLength(362);
+    rcsender.setRepeatTransmit(5);
+    Serial.print("  Transmitter PIN: ");
+    Serial.println(pin);
   }
+
+  void setupReceiver(int interrupt) {
+    rcmonitor.enableReceive(interrupt);  
+    Serial.print("  Receiver interrupt: ");
+    Serial.println(interrupt);
+  }
+
+  void setupSensor(uint8_t pin_sensor, int tolerance = 2) {
+    sensor.pin = pin_sensor;
+    sensor.value = analogRead(pin_sensor);
+    sensor.value_b = sensor.value;
+    sensor.tolerance = tolerance;
+    Serial.print("  Sensor analog PIN: ");
+    Serial.println(pin_sensor);
+  }
+
+  void setupSerial(unsigned long baud = 9600) {
+    Serial.begin(baud);
+    Serial.println("\nRCModul 433MHz using rcmonitor\n");
+  }
+
+  void setupAll() {
+    setupSerial();
+    setupSensor(SENSOR_PIN);
+    setupTransmitter(TRANSMITTER_PIN);
+    setupReceiver(RECEIVER_INT);
+    signalLED.start();
+  }
+
+  void wakeupTransmitter(uint8_t PIN_VCC, uint8_t PIN_DATA) {
+    Serial.println("  Transmitter wakeup!");  
+    digitalWrite(PIN_VCC, HIGH);    
+    rcsender.enableTransmit(PIN_DATA);
+    rcsender.setProtocol(1);
+    rcsender.setPulseLength(362);
+    rcsender.setRepeatTransmit(5);
+  }
+
+  void standbyTransmitter(uint8_t PIN_VCC) {
+    delayMicroseconds(100);
+    digitalWrite(PIN_VCC, LOW); 
+    Serial.println("  Transmitter standby!");  
+  }
+
+  void alive() {
+    signalLED.alive(1, 20);
+  }
+
+  int readSensor() {
+    sensor.value = analogRead(SENSOR_PIN);
+    return sensor.value;
+  }
+
+  bool releaseSensor() {
+    int delta = sensor.value - sensor.value_b;
+    if (abs(delta) > sensor.tolerance) { 
+      sensor.value_b = sensor.value;
+      sensor.delta = delta;
+      Serial.println("\n----");
+      Serial.print("  Sensor: Value ");
+      Serial.print(sensor.value);
+      Serial.print(", Delta ");
+      Serial.print(sensor.delta);
+      Serial.print(", Timer ");
+      Serial.println(rc_timer);
+      return true;
+    }
+    return false;
+  }
+
+  int getSensorValue() {
+    return sensor.value;
+  }
+
+  void sendData(const char* data) {
+    Serial.println("----");
+    wakeupTransmitter(TRANSMITTER_VCC, TRANSMITTER_PIN);
+    rcsender.send(data);
+    Serial.print("  Transmitter send data: ");
+    Serial.println(data);
+    standbyTransmitter(TRANSMITTER_VCC);
+  }
+
+  void sendDemo() {
+    rcsender.send("010100000101000101010001");
+    delay(5000);
+    rcsender.send("010100000101000101010100");
+    delay(10000);
+  }
+
+
 
   void monitor() {
-    if (rcswitch.available()) {
-      rcvalue = rcswitch.getReceivedValue();
-      rcbitlen = rcswitch.getReceivedBitlength();
-      rcdelay = rcswitch.getReceivedDelay();
-      rcrawdata = rcswitch.getReceivedRawdata();
-      rcprot = rcswitch.getReceivedProtocol();
-      Serial.print("Received Value: ");
-      Serial.println(rcvalue);
-      Serial.print("Received Bit length: ");
-      Serial.println(rcbitlen);
-      Serial.print("Received Protocol: ");
-      Serial.println(rcprot);
-      Serial.print("Received Delay: ");
-      Serial.println(rcdelay);
-      Serial.print("Received RAW Data: ");
+    if (rcmonitor.available()) {
+      rcvalue = rcmonitor.getReceivedValue();
+      rcbitlen = rcmonitor.getReceivedBitlength();
+      rcdelay = rcmonitor.getReceivedDelay();
+      rcrawdata = rcmonitor.getReceivedRawdata();
+      rcprot = rcmonitor.getReceivedProtocol();
+      Serial.println("----");
+      Serial.print("  Receiver: Value ");
+      Serial.print(rcvalue);
+      Serial.print(", Binary ");
       Serial.println(*rcrawdata, BIN);
+#ifdef DEBUG_DETAILS
       output(rcvalue, rcbitlen, rcdelay, rcrawdata,rcprot);
-      rcswitch.resetAvailable();
+#endif
+      rcmonitor.resetAvailable();
     }
   }
+
+  unsigned long loop() {
+    return rc_timer++;
+  }
+
+  boolean wait(long ticks) {
+    boolean res = !(rc_timer % ticks);
+    #ifdef DEBUG
+    if (res) {
+      Serial.print("  Debug Timer: ");
+      Serial.println(rc_timer);
+    }
+    #endif
+    return res;
+  }
+
+  void debug() {
+    Serial.print(digitalRead(PIN2));
+    Serial.print(",");
+  }
+
 };
 
-Funksteckdose funk;
+RCModul rcmodul;
 
 void setup() {
-  funk.init();
+  rcmodul.setupAll();
 }
 
-void loop() {
-  funk.monitor();
-  delay(10);
+void loop() {  
+  rcmodul.loop();
+  rcmodul.readSensor();
+  if (rcmodul.wait(10000) && rcmodul.releaseSensor()) {
+    if (rcmodul.getSensorValue() < 512) 
+      rcmodul.sendData("010100000101000101010100");
+    else      
+      rcmodul.sendData("010100000101000101010001");
+    rcmodul.alive();
+  }
+  rcmodul.monitor();
 }
 
 /*
