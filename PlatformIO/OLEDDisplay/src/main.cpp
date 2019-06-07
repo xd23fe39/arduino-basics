7f525d0b599cb72dc8a5232ff5ed05e5b87de6d9
xd23fe39 <xd23fe39@yahoo.de>
2019-06-07 18:32:40 +0200

    Update auf U8g2lib für SSD1306

--- PlatformIO/OLEDDisplay/src/main.cpp
+++ PlatformIO/OLEDDisplay/src/main.cpp
@@ -1,35 +1,91 @@
- #include <Arduino.h>
+#include <Arduino.h>
+#include <U8g2lib.h>
+#include <SPI.h>
+#include <Wire.h>
+
+#define LOGO "ON"
 
 /*
- * U8glib by oliver
- * 1.19.1
- * A library for monochrome TFTs and OLEDs
- */
- #include "U8glib.h"
 
-U8GLIB_SSD1306_128X32 u8g(U8G_I2C_OPT_NONE); // Just for 0.91”(128*32)
-//U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_NONE|U8G_I2C_OPT_DEV_0);// for 0.96” and 1.3”
+https://www.instructables.com/id/Tutorial-to-Interface-OLED-091inch-128x32-With-Ard/
 
-void printOLED(char* text, int y = 10) {
-  u8g.firstPage();  
-  do { 
-    u8g.drawStr(0, y, text);
-  } while (u8g.nextPage());
+*/
+
+// Display Controller: SSD1306
+U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C u8g2(U8G2_R0); 
+
+// Sample usage 
+unsigned long pc_start = millis();
+
+
+ void setup(void) {
+   Serial.begin(9600);
+
+   pinMode(LED_BUILTIN, OUTPUT);
+   digitalWrite(LED_BUILTIN, LOW);
+
+   u8g2.begin();
+
+#ifdef LOGO
+   u8g2.clearBuffer();					// clear the internal memory
+   u8g2.setFont(u8g2_font_logisoso18_tr);  // choose a suitable font at https://github.com/olikraus/u8g2/wiki/fntlistall
+   u8g2.drawStr(8,29,"XD23FE39");	// write something to the internal memory
+   u8g2.sendBuffer();					// transfer internal memory to the display
+   delay(3000);
+
+   u8g2.clearBuffer();         // clear the internal memory
+   u8g2.setFont(u8g2_font_logisoso18_tr);  // choose a suitable font at https://github.com/olikraus/u8g2/wiki/fntlistall
+   u8g2.drawStr(4,29,"BUS clock");  // write something to the internal memory
+   u8g2.sendBuffer();         // transfer internal memory to the display
+   delay(2000);
+
+   char clock[10];
+   sprintf(clock, "%.10d", (int) u8g2.getBusClock());
+   u8g2.clearBuffer();         // clear the internal memory
+   u8g2.setFont(u8g2_font_logisoso16_tr);  // choose a suitable font at https://github.com/olikraus/u8g2/wiki/fntlistall
+   u8g2.drawStr(10,29,clock);  // write something to the internal memory
+   u8g2.sendBuffer();         // transfer internal memory to the display
+   delay(2000);
+
+   u8g2.clearBuffer();         // clear the internal memory
+   u8g2.setFont(u8g2_font_logisoso18_tr);  // choose a suitable font at https://github.com/olikraus/u8g2/wiki/fntlistall
+   u8g2.drawStr(4,26,"System");  // write something to the internal memory
+   u8g2.sendBuffer();         // transfer internal memory to the display
+   delay(1000);
+
+   u8g2.clearBuffer();         // clear the internal memory
+   u8g2.setFont(u8g2_font_logisoso18_tr);  // choose a suitable font at https://github.com/olikraus/u8g2/wiki/fntlistall
+   u8g2.drawStr(4,26,"ready!");  // write something to the internal memory
+   u8g2.sendBuffer();         // transfer internal memory to the display
+   delay(2000);
+#endif
 }
 
-void setup(void) {
-  // flip screen, if required
-  // u8g.setRot180();
-  u8g.setFont(u8g_font_7x13);
-}
+  void loop(void) {
 
-char str[32];
+   digitalWrite(LED_BUILTIN, HIGH);
+   u8g2.clearBuffer();					// clear the internal memory
+   u8g2.drawHLine(14,18,100);
+   u8g2.sendBuffer();					// transfer internal memory to the display
+   delay(1000);
+   digitalWrite(LED_BUILTIN, LOW);
 
-void loop(void) {
-  sprintf(str, "Device: %s", "Arduino");
-  printOLED(str);
-  delay(2000);
-  sprintf(str, "Arduino: %s", __VERSION__);
-  printOLED(str);
-  delay(2000);
+   char pc_display[12];
+   sprintf(pc_display, "%ld ms", (unsigned long) millis() - pc_start);
+   u8g2.clearBuffer();         // clear the internal memory
+   u8g2.setFont(u8g2_font_logisoso16_tr);  // choose a suitable font at https://github.com/olikraus/u8g2/wiki/fntlistall
+   u8g2.drawStr(10,26,pc_display);  // write something to the internal memory
+   u8g2.sendBuffer();         // transfer internal memory to the display
+   delay(2000);
+   
+   double vcc_value = 3.3;
+   String vcc_str = ((vcc_value >=0) ? "+" : "-") + String(vcc_value) + " V";
+   char vcc_buf[10];
+   vcc_str.toCharArray(vcc_buf, 10);
+   u8g2.clearBuffer();					// clear the internal memory
+   u8g2.setFont(u8g2_font_logisoso24_tr);  // choose a suitable font at https://github.com/olikraus/u8g2/wiki/fntlistall
+   u8g2.drawStr(18,30,vcc_buf);	// write something to the internal memory
+   u8g2.sendBuffer();					// transfer internal memory to the display
+   delay(2000);
+   
 }
