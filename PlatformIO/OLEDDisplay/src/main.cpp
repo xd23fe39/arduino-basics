61b36809fe49c1a0376da50dc6f7cdb10611335e
xd23fe39 <xd23fe39@yahoo.de>
2019-06-07 22:25:50 +0200

    PIR Sensor Code hinzugefügt

--- PlatformIO/OLEDDisplay/src/main.cpp
+++ PlatformIO/OLEDDisplay/src/main.cpp
@@ -17,12 +17,22 @@ U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C u8g2(U8G2_R0);
 // Sample usage 
 unsigned long pc_start = millis();
 
+struct PIRSensor_T {
+   unsigned long timer;
+   int value;
+};
+
+PIRSensor_T pir1;
 
  void setup(void) {
    Serial.begin(9600);
 
    pinMode(LED_BUILTIN, OUTPUT);
    digitalWrite(LED_BUILTIN, LOW);
+
+   pinMode(PIN4, INPUT);
+   pir1.timer = millis() - pc_start;
+   pir1.value = digitalRead(PIN4);
 
    u8g2.begin();
 
@@ -69,6 +79,16 @@ unsigned long pc_start = millis();
    u8g2.sendBuffer();					// transfer internal memory to the display
    delay(1000);
    digitalWrite(LED_BUILTIN, LOW);
+
+   pir1.timer = millis() - pc_start;
+   pir1.value = digitalRead(PIN4);
+   char pir_display[12];
+   sprintf(pir_display, "%d PIR", pir1.value);
+   u8g2.clearBuffer();         // clear the internal memory
+   u8g2.setFont(u8g2_font_logisoso16_tr);  // choose a suitable font at https://github.com/olikraus/u8g2/wiki/fntlistall
+   u8g2.drawStr(10,26,pir_display);  // write something to the internal memory
+   u8g2.sendBuffer();         // transfer internal memory to the display
+   delay(2000);
 
    char pc_display[12];
    sprintf(pc_display, "%ld ms", (unsigned long) millis() - pc_start);
