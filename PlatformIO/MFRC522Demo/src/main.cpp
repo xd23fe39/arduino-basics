78ad43868fa87385727c66554c9e487e9e1d95e0
xd23fe39 <xd23fe39@yahoo.de>
2019-06-13 22:37:21 +0200

    Überarbeitet

--- PlatformIO/MFRC522Demo/src/main.cpp
+++ PlatformIO/MFRC522Demo/src/main.cpp
@@ -10,17 +10,27 @@
 #define RST_PIN 9 // RST an Pin 9 (UNO)
 
 void dump_byte_array(byte *buffer, byte bufferSize);
+void array_to_string(byte array[], unsigned int len, char buffer[]);
 
 MFRC522 mfrc522(SS_PIN, RST_PIN); // RFID-Empfänger initialisieren
 
 void setup() {
-  Serial.begin(9600); // Serielle Verbindung starten (Monitor)
+  // Initialisierung
+  Serial.begin(9600);     // Serielle Verbindung starten (Monitor)
+  SPI.begin();            // SPI-Verbindung aufbauen
+  mfrc522.PCD_Init();     // Initialisierung des RFID-Empfängers
 
-  SPI.begin(); // SPI-Verbindung aufbauen
-
-  mfrc522.PCD_Init(); // Initialisierung des RFID-Empfängers
+  // Ausgabe an Serial Monitor
   Serial.println("\nRFID MFRC522 Read/Write Modul Demo");
+
+  // Dump des Versionscodes / Versionsnummer  
   mfrc522.PCD_DumpVersionToSerial();
+
+  // Auslesen des Versionscodes aus Test-Registern des MFRC522 (0x92 == Version 2.0)
+  byte ver = mfrc522.PCD_ReadRegister(MFRC522::VersionReg);
+  Serial.print("\nMFRC522 VersionReg ");
+  Serial.print(ver,HEX);
+  Serial.println();
 }
 
 
@@ -30,12 +40,16 @@ void loop() {
   if ( mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial() ) {
 
     // Ausgabe an Serial Monitor startet
-    Serial.print("Die ID des RFID-TAGS lautet: ");
+    Serial.print("RFID-TAGS erkannt: ID ");
 
     // Auslesen...
-    dump_byte_array(mfrc522.uid.uidByte, mfrc522.uid.size);
+    dump_byte_array(mfrc522.uid.uidByte, mfrc522.uid.size);    
 
-    Serial.println(); // Mit dieser Zeile wird auf dem Serial Monitor nur ein Zeilenumbruch gemacht.
+    char code[48];
+    array_to_string(mfrc522.uid.uidByte, mfrc522.uid.size, code);
+    Serial.print(" - Code: ");
+    Serial.print(code);
+    Serial.println();
 
     delay(2000);
   }
@@ -49,4 +63,18 @@ void dump_byte_array(byte *buffer, byte bufferSize) {
     Serial.print(buffer[i] < 0x10 ? " 0" : " ");
     Serial.print(buffer[i], HEX);
   }
+  Serial.print(" Size ");
+  Serial.print(bufferSize);
+}
+
+void array_to_string(byte array[], unsigned int len, char buffer[])
+{
+    for (unsigned int i = 0; i < len; i++)
+    {
+        byte nib1 = (array[i] >> 4) & 0x0F;
+        byte nib2 = (array[i] >> 0) & 0x0F;
+        buffer[i*2+0] = nib1  < 0xA ? '0' + nib1  : 'A' + nib1  - 0xA;
+        buffer[i*2+1] = nib2  < 0xA ? '0' + nib2  : 'A' + nib2  - 0xA;
+    }
+    buffer[len*2] = '\0';
 }
